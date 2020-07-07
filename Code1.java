6da5cce6461f11823c6563a7ee1edb819805f4e2
hubblin <43435547+hubblin@users.noreply.github.com>
2020-07-07 13:05:17 +0900

    Add

---
+++ Code1.java
@@ -0,0 +1,33 @@
+public class Code1{
+    /* 1. 문제
+    10미만의 자연수에서 3과 5의 배수를 구하면 3,5,6,9이다. 이들의 총합은 23이다.
+    1000미만의 자연수에서 3,5,의 배수의 총합을 구하라 */
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i =1; i < 1000; i++){
+            if(three(i) == five(i) && three(i) != 0 && five(i) != 0){
+
+                sum += i;
+            }else if(three(i) != 0){
+
+                sum += i;
+            }else if(five(i) != 0){
+
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+    public static int three(int num){
+        if(num % 3 == 0){
+            return num;
+        }
+        return 0;
+    } 
+    public static int five(int num){
+        if(num % 5 == 0){
+            return num;
+        }
+        return 0;
+    }
+}
